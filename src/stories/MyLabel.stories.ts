1b1bbb7e95657ec73e3b63bf0f0da73d3e66d739
juan_gomez9905 <juan.gomez@bebolder.co>
2025-05-22 14:56:55 -0500

    preparing project to allow use like a library of components

--- src/stories/MyLabel.stories.ts
+++ src/stories/MyLabel.stories.ts
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { MyLabel } from "../components/MyLabel";
+import { MyLabel, type Props as MyLAbelProps } from "../components/MyLabel";
 
-const meta = {
+const meta: Meta<MyLAbelProps> = {
     title: 'UI/labels/MyLabel',
     component: MyLabel,
     tags: ['autodocs'],
